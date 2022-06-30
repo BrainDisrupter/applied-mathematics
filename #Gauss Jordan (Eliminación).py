5dc84cd139550ce49a28c3fc3e3713c310ef6328
Claudio Palacio <a181156@lazarocardenas.edu.mx>
2022-06-30 13:24:54 -0700

    Update #Gauss Jordan (Eliminación).py

--- #Gauss Jordan (Eliminación).py
+++ #Gauss Jordan (Eliminación).py
@@ -38,7 +38,7 @@ while opc==1:
                     A[0],A[i] = A[i],A[0]
                     break
                 i+=1
-            print(f"INTERCAMBIAMOS RENGLON 1 POR RENGLON {i+1} DEBIDO AL CERO INICIAL")
+            print(f"\nINTERCAMBIAMOS RENGLON 1 POR RENGLON {i+1} DEBIDO AL CERO INICIAL")
             imprimirmatriz(A)
         for i in range(fil): #Procedimiento Gauss-Jordan (Eliminación)
             pivote = A[i][i]
@@ -49,29 +49,28 @@ while opc==1:
                     A[i][j] = 0
             print("\nMATRIZ DE REFERENCIA")
             imprimirmatriz(A)
-            print(f"RENGLÓN PIVOTE: R{i+1}. DIVIDIDO ENTRE {pivote}")
             for l in range(fil):
                 if (l != i):
                     multiplo = A[l][i]
                     print(f"R{l+1} - R{i+1} x {multiplo} => R{l+1}")
                     for k in range(col):
                         A[l][k] = A[l][k]-(A[i][k]*multiplo)
-            print("MATRIZ NUEVA")
+            print("\nMATRIZ NUEVA")
             imprimirmatriz(A)
-            print("*****************\n")
+            print("************************************")
         if (A[fil-1][col-2]==1): #Si a la izquierda hay un 1 hasta la parte inferior, decimos que tiene solución única.
             print("\nEste S.E.L tiene solución única.")
         elif(A[fil-1][col-2]==0 and A[fil-1][col-1]==0): #Si los 2 ultimos valores de la parte inferior son 0, decimos que tiene soluciones infinitas.
             print("\nEste S.E.L tiene soluciones infinitas.")
         elif(A[fil-1][col-2]==0 and A[fil-1][col-1]!=0): #Si el último valor del último renglón es diferente de 0, pero el izquierdo es igual a 0, decimos que el S.E.L no tiene solución.
             print("\nEste S.E.L no tiene solución.")
-        print("INCÓGNITAS: ")
+        print("\nINCÓGNITAS: ")
         for i in range(fil): #Llenado de vector solución
             vecsolu.append(A[i][fil])
             print(f"X{i+1}: {A[i][fil]}")
-        print("VECTOR SOLUCION: ")
+        print("\nVECTOR SOLUCION: ")
         print(vecsolu)
-        print("MATRIZ RESUELTA")
+        print("\nMATRIZ RESUELTA")
         imprimirmatriz(A)
     else:
         opc = int(input("La diferencia entre renglones y columnas no es adecuada, el S.E.L sería irresoluble. \n¿Desea volver a introducir los datos? \n1.Sí 2.No: "))
