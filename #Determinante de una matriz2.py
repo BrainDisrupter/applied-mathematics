9d41ff577914e50b0853a98f5cd9fd9f772fb1a7
Claudio Palacio <a181156@lazarocardenas.edu.mx>
2022-07-03 03:20:38 -0700

    update
    añado cambio de renglones si llegase a poner en el valor A1,1 un 0, el programa cambiara el renglon por uno que tenga en la 1ra columna uno diferente de 0
    
    tambien añado print que señala los multiplos necesarios para cada renglon para igualarlos a 0 en sus columnas correspondientes segun el renglon pivote donde nos hallamos

--- #Determinante de una matriz2.py
+++ #Determinante de una matriz2.py
@@ -24,12 +24,22 @@ while opc==1:
                     continue
             renglon.append(num)
         A.append(renglon)
+    if (A[0][0] == 0): #Dado que hubiera un 0 en A1,1 en el primer renglon, lo intercambiamos por uno que tenga un valor diferente de 0 en su primera columna.
+        i=1
+        while i<recorrido:
+            if (A[i][0]!=0):
+                A[0],A[i] = A[i],A[0]
+                break
+            i+=1
+        print(f"\nINTERCAMBIAMOS RENGLON 1 POR RENGLON {i+1} DEBIDO AL CERO INICIAL")
+        imprimirmatriz(A)
     for i in range(recorrido): #Procedimiento Gauss-Jordan (Eliminación)
         print("\nMATRIZ DE REFERENCIA")
         imprimirmatriz(A)
         for l in range(i,recorrido):
             if (l != i):
                 divisor = A[l][i]/A[i][i]*(-1)
+                print(f"R{l+1} - R{i+1} x {divisor} => R{l+1}")
                 for k in range(i,recorrido):
                     A[l][k] = A[i][k]*divisor + A[l][k]
     sumdet = 1
@@ -38,6 +48,6 @@ while opc==1:
     if (sumdet == -0.0):
        sumdet = 0.0
     print(f"La determinante es: {sumdet}")
-    opc = int(input("\n¿Deseas calcular la determinante de otra matriz? \n1. Sí 2. No"))
+    opc = int(input("\n¿Deseas calcular la determinante de otra matriz? \n1. Sí 2. No: "))
 print("*******************")
 imprimirmatriz(A)
